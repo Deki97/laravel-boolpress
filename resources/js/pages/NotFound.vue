9d5e79fbc3092b9b6159f0aff7202458d40bdbb0
Deki97 <dejan97@libero.it>
2022-03-03 15:55:01 +0100

    done 404 component not found + return back link

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <img class="mt-5" height="150px" width="150px" src="https://cdn-icons-png.flaticon.com/512/1548/1548682.png" alt="Not found icon">
+        <h2>Ops..la ricerca non ha prodotto nessun risultato</h2>
+
+        <router-link class="nav-link" :to="{ name: 'blog' }">Ritorna alla sezione dei blog</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter);
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 import Blog from './pages/Blog.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = new VueRouter({
     mode: "history",
@@ -25,6 +26,11 @@ const router = new VueRouter({
             name: "blog",
             component: Blog
         },
+        {
+            path: "/*",
+            name: "not-found",
+            component: NotFound
+        }
     ]
 });
 
