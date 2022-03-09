9415b7d2048243ed90de3f8e3f9dcec72604fd1d
Deki97 <dejan97@libero.it>
2022-03-09 11:20:58 +0100

    validation api

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,16 +7,35 @@ use Illuminate\Http\Request;
 use App\Lead;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\NewContactMail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request) {
         $data = $request->all();
+        
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'name' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'succcess' => false,
+                'errors' =>$validator->errors()
+            ]);
+        }
+        
 
         $new_lead = new Lead();
         $new_lead->fill($data);
         $new_lead->save();
 
         Mail::to('customerservice@boolpress.it')->send(new NewContactMail($new_lead));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
