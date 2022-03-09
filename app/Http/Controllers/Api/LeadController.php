3527715e38bb9cecca0c3bb7c5b390ce60db58d0
Deki97 <dejan97@libero.it>
2022-03-09 10:10:29 +0100

    created lead mail

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Lead;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewContactMail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request) {
+        $data = $request->all();
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        Mail::to('customerservice@boolpress.it')->send(new NewContactMail($new_lead));
+    }
+}
